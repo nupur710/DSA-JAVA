59aadb08fe9f1ae745e4ffc618f4a89ca4d2ecd9
Nupur Singh <nupur.singh2410@gmail.com>
2024-03-10 21:06:42 +0530

    Height of Tree, BFS traversal

--- src/main/java/tree/Node.java
+++ src/main/java/tree/Node.java
@@ -1,5 +1,8 @@
 package tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Node {
 
     int key;
@@ -15,6 +18,7 @@ public class Node {
         root.leftChild= new Node(20);
         root.rightChild= new Node(30);
         root.leftChild.leftChild= new Node(80);
+        root.leftChild.leftChild.leftChild= new Node(40);
         System.out.println("Inorder traversal: ");
         inOrderTraversal(root);
         System.out.println();
@@ -23,6 +27,13 @@ public class Node {
         System.out.println();
         System.out.println("Postorder traversal: ");
         postorderTraversal(root);
+        System.out.println();
+        System.out.println("Height of tree is: " + height(root));
+        System.out.print("Node at distance k is ");
+        nodesAtDistanceK(root, 3);
+        System.out.println();
+        System.out.println("Level order traversal: ");
+        levelOrderTraversal(root);
     }
 
     //inorder traversal--> left-root-right
@@ -47,6 +58,32 @@ public class Node {
             postorderTraversal(root.leftChild);
             postorderTraversal(root.rightChild);
             System.out.print(root.key+ " ");
+        }
+    }
+    //height of tree
+    public static int height(Node root) {
+        if(root==null) return 0;
+        else {
+            return Math.max(height(root.leftChild),height(root.rightChild))+1;
+        }
+    }
+    public static void nodesAtDistanceK(Node root, int k) {
+        if(root == null) return;
+        if(k == 0) System.out.print(root.key + " ");
+        else {
+            nodesAtDistanceK(root.leftChild, k-1);
+            nodesAtDistanceK(root.rightChild, k-1);
+        }
+    }
+    //breadth-first traversal
+    public static void levelOrderTraversal(Node root) {
+        Queue<Node> q= new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()) {
+            Node curr= q.poll();
+            System.out.print(curr.key + " ");
+            if(curr.leftChild != null) q.add(curr.leftChild);
+            if(curr.rightChild != null) q.add(curr.rightChild);
         }
     }
 }
